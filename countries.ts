8bf2a10334ee17509266de34180d6a8eb20219a9
Phil Kunz <github@philkunz.com>
2023-07-14 07:58:04 +0200

    Create countries.ts

---
+++ countries.ts
@@ -0,0 +1,71 @@
+let largestAndEuropeanCountries: string[] = [
+  'RU', // Russia
+  'CN', // China
+  'CA', // Canada
+  'US', // United States
+  'BR', // Brazil
+  'AU', // Australia
+  'IN', // India
+  'AR', // Argentina
+  'KZ', // Kazakhstan
+  'DZ', // Algeria
+  'CD', // Democratic Republic of the Congo
+  'SA', // Saudi Arabia
+  'MX', // Mexico
+  'ID', // Indonesia
+  'SD', // Sudan
+  'LY', // Libya
+  'IR', // Iran
+  'MN', // Mongolia
+  'PE', // Peru
+  'TD', // Chad
+  'AL', // Albania
+  'AD', // Andorra
+  'AM', // Armenia
+  'AT', // Austria
+  'AZ', // Azerbaijan
+  'BY', // Belarus
+  'BE', // Belgium
+  'BA', // Bosnia and Herzegovina
+  'BG', // Bulgaria
+  'HR', // Croatia
+  'CY', // Cyprus
+  'CZ', // Czech Republic
+  'DK', // Denmark
+  'EE', // Estonia
+  'FI', // Finland
+  'FR', // France
+  'GE', // Georgia
+  'DE', // Germany
+  'GR', // Greece
+  'HU', // Hungary
+  'IS', // Iceland
+  'IE', // Ireland
+  'IT', // Italy
+  'XK', // Kosovo
+  'LV', // Latvia
+  'LI', // Liechtenstein
+  'LT', // Lithuania
+  'LU', // Luxembourg
+  'MT', // Malta
+  'MD', // Moldova
+  'MC', // Monaco
+  'ME', // Montenegro
+  'NL', // Netherlands
+  'MK', // North Macedonia
+  'NO', // Norway
+  'PL', // Poland
+  'PT', // Portugal
+  'RO', // Romania
+  'SM', // San Marino
+  'RS', // Serbia
+  'SK', // Slovakia
+  'SI', // Slovenia
+  'ES', // Spain
+  'SE', // Sweden
+  'CH', // Switzerland
+  'TR', // Turkey
+  'UA', // Ukraine
+  'GB', // United Kingdom
+  'VA'  // Vatican City (Holy See)
+];
